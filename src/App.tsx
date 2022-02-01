b88eba87dc1aec7a939a0743624690d490c0ea4b
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-01 17:47:40 -0300

    Improvemented routes organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import LoginPage from './components/Login';
+import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
-import { BrowserRouter, Switch } from 'react-router-dom';
 import PublicRoute from './routes/Public';
 import PrivateRoute from './routes/Private';
+
+import LoginPage from './components/Login';
+import AppPage from './components/App';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <PrivateRoute exact path="/" component={() => {
-          return <h1>Authenticated</h1>
-        }} />
-        <PublicRoute exact path="/login" component={LoginPage} restricted={true} />
+        <PublicRoute exact path="/">
+          <Redirect to="/app" />
+        </PublicRoute>
+        <PrivateRoute path="/app" component={AppPage} />
+        <PublicRoute path="/login" component={LoginPage} restricted={true} />
       </Switch>
     </BrowserRouter>
   );
