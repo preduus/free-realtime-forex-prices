6f653c8dbdb72910f9769584372270f87546af4a
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-02-01 11:08:13 -0300

    Created routers switch on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import LoginPage from './components/Login';
+
+import { BrowserRouter, Switch } from 'react-router-dom';
+import PublicRoute from './routes/Public';
+import PrivateRoute from './routes/Private';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path="/" component={() => {
+          return <h1>Authenticated</h1>
+        }} />
+        <PublicRoute exact path="/login" component={LoginPage} restricted={true} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
